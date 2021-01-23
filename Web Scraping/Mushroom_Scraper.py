ab7fb7df42552b96a05398d0b1271bf5e4401a0b
Saul Alarcon <alarcon.saul@gmail.com>
2021-01-23 17:55:44 -0600

    Starting to Build out Scraping File

--- Web Scraping/Mushroom_Scraper.py
+++ Web Scraping/Mushroom_Scraper.py
@@ -1,1 +1,12 @@
-# Python Script for Scraping Data from http://www.mushroom.world/mushrooms/list
+# Python Script for Scraping Data from http://www.mushroom.world/mushrooms/namelist
+
+
+import scrapy
+
+
+class mushroom_scraper(scrapy.Spider):
+    name = 'posts'
+
+    start_urls = ['http://www.mushroom.world/mushrooms/namelist']
+
+    
